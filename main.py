aacaf1a48f5f96c8957eb0d1266ea9f2c9bc4a72
stephen-jr <stephen24jnr@gmail.com>
2020-12-17 16:27:35 +0100

    build Complete

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-from data_utils import *
 from model import BiLSTM
+
+model = BiLSTM(variant='new', _dir=None)
+# model = BiLSTM(variant='load', _dir='models/2020-12-17')
+model.input_data(variant='train', path='data/train.csv')
+# model.input_data(variant='predict', path='data/train.csv')
+model.tokenize(variant='train', text=None)
+# model.tokenize(variant='predict', text=None)
+# tokenized_text =  model.tokenize(text='I love Bidirectional LSTM Networks with Self Attention')
+model.create_model(embedding=True)
+# model.create_model(embedding=False)
+history, train_accuracy, train_precision, train_recall, train_fscore = model.train()
+model.create_checkpoint()
+test_accuracy, test_precision, test_recall, test_fscore = model.validate()
+prediction = model.predict(data='I love Bidirctional LSTM Networks')
+# prediction = model.predict(internal=True)  # Predict on inputed predict data
--- model.py
+++ model.py
@@ -1,141 +1,180 @@
-import pandas as pd
-import numpy as np
-
-from keras import backend as K
-from keras.models import Sequential, Model
-from keras.layers import Flatten, Dense, Embedding, Input, LSTM, Bidirectional, Dropout
-from keras.preprocessing.sequence import pad_sequences
-from keras.preprocessing.text import Tokenizer
-from keras.callbacks import EarlyStopping, ModelCheckpoint
+from data_utils import *
 
 
+class BiLSTM:
+    def __init__(self, variant, _dir):
+        if variant is 'new':
+            self.prediction_data = None
+            self.model = None
+            self.X = self.Y = None
+            self.X_Train = self.X_Test = None
+            self.Y_Train = self.Y_Test = None
+            self.tokenizer = None
+            self.embedding_dimension = 300
+            self.embedding_matrix = None
+            self.vocab_size = None
+            self.maxlen = None
+        elif variant is 'load':
+            if _dir:
+                dependencies = {
+                    'precision_m': precision_m,
+                    'recall_m': recall_m,
+                    'f1_m': f1_m
+                }
+                self.model = load_model(_dir + '/model.h5', custom_objects=dependencies)
+                with open(_dir + '/tokenizer.pkl', 'rb') as f:
+                    self.tokenizer = dill.load(f)
+                with open(_dir + '/variables.pkl', 'rb') as f:
+                    var = dill.load(f)
+                if var:
+                    self.maxlen = var.maxlen
 
-class Attention(Layer):
-    def __init__(self, step_dim,
-                 W_regularizer=None, b_regularizer=None,
-                 W_constraint=None, b_constraint=None,
-                 bias=True, **kwargs):
-        self.supports_masking = True
-        self.init = initializers.get('glorot_uniform')
+    @staticmethod
+    def preprocess(text):
+        punc = string.punctuation.replace('.', '').replace(',', '')
+        text = text.lower()
+        text = re.sub(r'http\S+', ' ', text)
+        text = re.sub(r"i'm", "i am", text)
+        text = re.sub(r"he's", "he is", text)
+        text = re.sub(r"she's", "she is", text)
+        text = re.sub(r"that's", "that is", text)
+        text = re.sub(r"where's", "where is", text)
+        text = re.sub(r"what's", "what is", text)
+        text = re.sub(r"\'ll", ' will', text)
+        text = re.sub(r"\'ve", ' have', text)
+        text = re.sub(r"\'d", ' would', text)
+        text = re.sub(r"won't", "will not", text)
+        text = re.sub(r"can't", "cannot", text)
+        text = re.sub(r"\'re", ' are', text)
+        text = re.sub(r'\(\d+\)', ' ', text)
+        text = re.sub(r'pic.\S+', ' ', text)
+        text = re.sub(r'@\s\w+', ' ', text)
+        text = re.sub(r'#', ' ', text)
+        text = re.compile(r'<[^>]+>').sub(' ', text)
+        text = re.sub(r'[' + punc + ']', ' ', text)
+        text = re.sub(r'RT', ' ', text)
+        text = re.sub(r'\s+', ' ', text)
+        return text.strip()
 
-        self.W_regularizer = regularizers.get(W_regularizer)
-        self.b_regularizer = regularizers.get(b_regularizer)
+    def input_data(self, variant, path):
+        f_xtsn = path.split(".")[-1]
+        dataFrame = None
+        if f_xtsn is 'csv':
+            dataFrame = pd.read_csv(path, error_bad_lines=False, encoding='ISO-8859-1')
+        elif f_xtsn is 'json':
+            dataFrame = pd.read_json(path, lines=True)
+        if variant is 'train':
+            self.X = dataFrame['text'].astype('str').appy(self.preprocess)
+            self.Y = dataFrame['sentiment']
+            self.X_Train, self.X_Test, self.Y_Train, self.Y_Test = train_test_split(self.X, self.Y, test_size=0.25,
+                                                                                    random_state=42)
+        elif variant is 'predict':
+            self.prediction_data = dataFrame['text'].astype('str').appy(self.preprocess)
 
-        self.W_constraint = constraints.get(W_constraint)
-        self.b_constraint = constraints.get(b_constraint)
+    def tokenize(self, variant, text):
+        start = time()
+        if variant:
+            if variant is 'train':
+                self.tokenizer.fit_on_texts(self.X_Train)
+                self.X_Train = self.tokenizer.texts_to_sequences(self.X_Train)
+                self.X_Test = self.tokenizer.texts_to_sequences(self.X_Test)
+                self.vocab_size = len(self.tokenizer.word_index) + 1
+                print("Found {} unique words".format(self.vocab_size))
+                print('Tokenization time taken : {}s'.format(time() - start))
+                self.maxlen = max(len(x) for x in self.X_Train)
+                start = time()
+                self.X_Train = pad_sequences(self.X_Train, padding='post', maxlen=self.maxlen)
+                self.X_Test = pad_sequences(self.X_Test, padding='post', maxlen=self.maxlen)
+                print('Padding Time taken : {}s'.format(time() - start))
+            elif variant is 'predict':
+                self.prediction_data = self.tokenizer.texts_to_sequences(self.prediction_data)
+                self.prediction_data = pad_sequences(self.prediction_data, padding='post', maxlen=self.maxlen)
+                print('Time taken : {}s'.format(time() - start))
+        if text:
+            text = [text]
+            text = self.tokenizer.texts_to_sequences(text)
+            text = pad_sequences(text, padding='post', maxlen=self.maxlen)
+            print('Time taken : {}s'.format(time() - start))
+            return text
 
-        self.bias = bias
-        self.step_dim = step_dim
-        self.features_dim = 0
-        super(Attention, self).__init__(**kwargs)
+    def initialize_embedding(self):
+        emb = api.load('word2vec-google-news-300')
+        self.embedding_matrix = np.zeros((self.vocab_size, self.embedding_dimension))
+        for word, index in tqdm(self.tokenizer.word_index.items(),
+                                desc="Word2Vec Embedding Vector"):
+            try:
+                embedding_vector = emb[word]
+            except KeyError:
+                continue
+            if embedding_vector is not None:
+                self.embedding_matrix[index] = embedding_vector
 
-    def build(self, input_shape):
-        assert len(input_shape) == 3
-
-        self.W = self.add_weight((input_shape[-1],),
-                                 initializer=self.init,
-                                 name='{}_W'.format(self.name),
-                                 regularizer=self.W_regularizer,
-                                 constraint=self.W_constraint)
-        self.features_dim = input_shape[-1]
-
-        if self.bias:
-            self.b = self.add_weight((input_shape[1],),
-                                     initializer='zero',
-                                     name='{}_b'.format(self.name),
-                                     regularizer=self.b_regularizer,
-                                     constraint=self.b_constraint)
+    def create_model(self, embedding):
+        self.model = Sequential()
+        if embedding:
+            self.initialize_embedding()
+            self.model.add(Embedding(self.vocab_size, self.embedding_dimension, weights=[self.embedding_matrix],
+                                     input_length=self.maxlen, trainable=False))
         else:
-            self.b = None
+            self.model.add(Embedding(self.vocab_size, self.embedding_dimension, input_length=self.maxlen))
+        self.model.add(
+            Bidirectional(LSTM(256, return_sequences=True), input_shape=(self.maxlen, self.embedding_dimension)))
+        self.model.add(SeqSelfAttention(attention_activation='sigmoid'))
+        # self.model.add(BatchNormalization())
+        self.model.add(Dropout(0.5))
+        self.model.add(Dense(1, activation='sigmoid'))
+        self.model.compile(optimizer='adam', loss='binary_crossentropy',
+                           metrics=['accuracy', precision_m, recall_m, f1_m])
+        self.model.summary()
 
-        self.built = True
+    def train(self):
+        es = EarlyStopping(monitor='val_loss', mode='min', patience=2, verbose=1)
+        history = self.model.fit(self.X_Train,
+                                 self.Y_Train,
+                                 epochs=20,
+                                 verbose=True,
+                                 validation_data=(self.X_Test, self.Y_Test),
+                                 batch_size=128,
+                                 callbacks=[es])
+        _, train_accuracy, train_precision, train_recall, train_fscore = self.model.evaluate(self.X_Train,
+                                                                                             self.Y_Train,
+                                                                                             verbose=True)
+        print("Training Accuracy: {:.4f}".format(train_accuracy))
+        print("Training metrics")
+        print(f'Precision : {train_precision:{5}} Recall : {train_recall:{5}} F-Score : {train_fscore:{5}}')
+        return history, train_accuracy, train_precision, train_recall, train_fscore
 
-    def compute_mask(self, input, input_mask=None):
-        return None
+    def create_checkpoint(self):
+        dte = str(date.today())
+        ckpt_dir = '/models/' + dte
+        os.makedirs(ckpt_dir)
+        self.model.save(ckpt_dir + '/model.h5')
+        with open(ckpt_dir + '/tokenizer.pkl', 'wb') as f:
+            dill.dump(self.tokenizer, f)
+        with open(ckpt_dir + '/variables.pkl', 'wb') as f:
+            dill.dump({
+                'maxlen': self.maxlen
+            }, f)
 
-    def call(self, x, mask=None):
-        features_dim = self.features_dim
-        step_dim = self.step_dim
+    def validate(self):
+        _, test_accuracy, test_precision, test_recall, test_fscore = self.model.evaluate(self.X_Test,
+                                                                                         self.Y_Test,
+                                                                                         verbose=True)
+        print("Testing Accuracy:  {:.4f}".format(test_accuracy))
+        print("Testing metrics")
+        print(f'Precision : {test_precision:{5}} Recall : {test_recall:{5}} F-Score : {test_fscore:{5}}')
+        return test_accuracy, test_precision, test_recall, test_fscore
 
-        eij = K.reshape(K.dot(K.reshape(x, (-1, features_dim)),
-                        K.reshape(self.W, (features_dim, 1))), (-1, step_dim))
-
-        if self.bias:
-            eij += self.b
-
-        eij = K.tanh(eij)
-
-        a = K.exp(eij)
-
-        if mask is not None:
-            a *= K.cast(mask, K.floatx())
-
-        a /= K.cast(K.sum(a, axis=1, keepdims=True) + K.epsilon(), K.floatx())
-
-        a = K.expand_dims(a)
-        weighted_input = x * a
-        return K.sum(weighted_input, axis=1)
-
-    def compute_output_shape(self, input_shape):
-        return input_shape[0],  self.features_dim
-
-
-def BiLstm(maxlen, max_features, embed_size, embedding_matrix):
-    inp = Input(shape=(maxlen, ))
-    x = Embedding(max_features, embed_size, weights=[embedding_matrix],
-                  trainable=False)(inp)
-    x = Bidirectional(LSTM(300, return_sequences=True, dropout=0.25,
-                           recurrent_dropout=0.25))(x)
-    x = Attention(maxlen)(x)
-    x = Dense(256, activation="relu")(x)
-    x = Dropout(0.25)(x)
-    x = Dense(6, activation="sigmoid")(x)
-    model = Model(inputs=inp, outputs=x)
-
-    return model
-    
-    
-def make_df(train_path, test_path, max_features, maxlen, list_classes):
-    train = pd.read_csv(train_path)
-    test = pd.read_csv(test_path)
-    train = train.sample(frac=1)
-
-    list_sentences_train = train["comment_text"].fillna("unknown").values
-    y = train[list_classes].values
-    list_sentences_test = test["comment_text"].fillna("unknown").values
-
-    tokenizer = Tokenizer(num_words=max_features)
-    tokenizer.fit_on_texts(list(list_sentences_train))
-    list_tokenized_train = tokenizer.texts_to_sequences(list_sentences_train)
-    list_tokenized_test = tokenizer.texts_to_sequences(list_sentences_test)
-    X_train = pad_sequences(list_tokenized_train, maxlen=maxlen)
-    X_test =pad_sequences(list_tokenized_test, maxlen=maxlen)
-
-    word_index = tokenizer.word_index
-
-    return X_t, X_te, y, word_index
-    
-max_features = 100000
-maxlen = 150
-embed_size = 300
-list_classes = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
-
-xtr, xte, y, word_index = make_df("../input/jigsaw-toxic-comment-classification-challenge/train.csv", 
-                                  "../input/jigsaw-toxic-comment-classification-challenge/test.csv", 
-                                  max_features, 
-                                  maxlen, 
-                                  list_classes)
-embedding_vector = make_glovevec("../input/glove840b300dtxt/glove.840B.300d.txt", 
-                                 max_features, 
-                                 embed_size, 
-                                 word_index)
-
-model = BidLstm(maxlen, max_features, embed_size, embedding_vector)
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-file_path = ".model.hdf5"
-ckpt = ModelCheckpoint(file_path, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
-early = EarlyStopping(monitor="val_loss", mode="min", patience=1)
-model.fit(xtr, y, batch_size=256, epochs=15, validation_split=0.1, callbacks=[ckpt, early])
-
-model.load_weights(file_path)
-y_test = model.predict(xte)
+    def predict(self, internal, data):
+        if internal:
+            pred = self.model.predict(self.prediction_data)
+        else:
+            if isinstance(data, list):
+                data = [self.preprocess(x) for x in data]
+            if isinstance(data, str):
+                data = self.preprocess(data)
+                data = [data]
+            text = self.tokenizer.texts_to_sequences(data)
+            text = pad_sequences(text, padding='post', maxlen=self.maxlen)
+            pred = self.model.predict(text)
+        return pred
